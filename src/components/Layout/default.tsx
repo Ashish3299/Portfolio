aa3577c77b2e2471cf44e9a98b1014351fda4ea9
Ashish <ashish15731@gmail.com>
2024-06-24 01:36:21 +0530

    Created page loader

--- src/components/Layout/default.tsx
+++ src/components/Layout/default.tsx
@@ -1,15 +1,77 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import Loader from "../Loader";
 
 type Props = {
-  children: any
-}
+  children: any;
+  location: any;
+};
 
-const Layout: React.FC<Props>  = ({children}) => {
+const Layout: React.FC<Props> = ({ children, location }) => {
+  const isHome = location.pathname === '/';
+  const [isLoading, setIsLoading] = useState(isHome);
+
+  // Sets target="_blank" rel="noopener noreferrer" on external links
+  const handleExternalLinks = () => {
+    const allLinks = Array.from(document.querySelectorAll('a'));
+    if (allLinks.length > 0) {
+      allLinks.forEach(link => {
+        if (link.host !== window.location.host) {
+          link.setAttribute('rel', 'noopener noreferrer');
+          link.setAttribute('target', '_blank');
+        }
+      });
+    }
+  };
+
+  useEffect(() => {
+    if (isLoading) {
+      return;
+    }
+
+    if (location.hash) {
+      const id = location.hash.substring(1); // location.hash without the '#'
+      setTimeout(() => {
+        const el = document.getElementById(id);
+        if (el) {
+          el.scrollIntoView();
+          el.focus();
+        }
+      }, 0);
+    }
+
+    handleExternalLinks();
+  }, [isLoading]);
   return (
     <>
-      {children}
-    </>
-  )
-}
+      <div id="root">
+        <a className="skip-to-content" href="#content">
+          Skip to Content
+        </a>
 
-export default Layout
+        {isLoading && isHome ? (
+          <Loader finishLoading={() => setIsLoading(false)} />
+        ) : (
+          <StyledContent>
+            {/* <Nav isHome={isHome} />
+            <Social isHome={isHome} />
+            <Email isHome={isHome} /> */}
+
+            <div id="content">
+              {children}
+              {/* <Footer /> */}
+            </div>
+          </StyledContent>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default Layout;
+
+const StyledContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+`;
