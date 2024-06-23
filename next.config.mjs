0637649a5bea92672ddfe6dfa28fe4d0413c405d
Ashish <ashish15731@gmail.com>
2024-06-23 21:02:30 +0530

    added loader for the fonts.

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,26 @@ const nextConfig = {
   reactStrictMode: true,
   compiler: {
     styledComponents: true
-  }
+  },
+  webpack(config, options) {
+    config.module.rules.push({
+      test: /\.(woff|woff2|eot|ttf|otf)$/i,
+      use: [
+        {
+          loader: 'file-loader',
+          options: {
+            outputPath: 'static/fonts/',
+            publicPath: '/_next/static/fonts/',
+            name: '[path][name].[ext]',
+            context: 'src/', // This makes sure the [path] includes the subdirectories
+          },
+        },
+      ],
+    });
+
+    return config;
+  },
+  
 };
 
 export default nextConfig;
--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -1,11 +1,12 @@
 import { createGlobalStyle } from 'styled-components';
-// import fonts from './fonts';
+import fonts from './fonts';
 import variables from './variables';
 import TransitionStyles from './TransitionStyles';
 import PrismStyles from './PrismStyles';
 
 const GlobalStyle = createGlobalStyle`
   ${variables};
+  ${fonts}
 
   html {
     box-sizing: border-box;
