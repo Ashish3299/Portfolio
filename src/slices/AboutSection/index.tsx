8ef5370895c4ebcdcb1fdf5a5c478115fc280681
Ashish <ashish15731@gmail.com>
2024-06-25 17:47:50 +0530

    Completed dynamic of the about section slice.

--- src/slices/AboutSection/index.tsx
+++ src/slices/AboutSection/index.tsx
@@ -1,5 +1,12 @@
+import React, { useEffect, useRef } from "react";
+import Image from "next/image";
+import styled from "styled-components";
+import usePrefersReducedMotion from "@/hooks/usePrefersReducedMotion";
+import { srConfig } from "@/config";
+// import sr from "@/utils/sr";
 import { Content } from "@prismicio/client";
-import { SliceComponentProps } from "@prismicio/react";
+import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
+import { PrismicNextImage } from "@prismicio/next";
 
 /**
  * Props for `AboutSection`.
@@ -10,15 +17,154 @@ export type AboutSectionProps = SliceComponentProps<Content.AboutSectionSlice>;
  * Component for "AboutSection" Slices.
  */
 const AboutSection = ({ slice }: AboutSectionProps): JSX.Element => {
+  const { heading, description, image, technologies } = slice.primary;
+  console.log(technologies);
+
+  const revealContainer = useRef<HTMLElement>(null);
+  const prefersReducedMotion = usePrefersReducedMotion();
+
+  useEffect(() => {
+    if (prefersReducedMotion) {
+      return;
+    }
+    // sr.reveal(revealContainer.current, srConfig());
+  }, []);
+
   return (
-    <section
-      data-slice-type={slice.slice_type}
-      data-slice-variation={slice.variation}
-    >
-      Placeholder component for about_section (variation: {slice.variation})
-      Slices
-    </section>
+    <StyledAboutSection id="about" ref={revealContainer}>
+      <h2 className="numbered-heading">{heading}</h2>
+
+      <div className="inner">
+        <StyledText>
+          <PrismicRichText field={description} />
+
+          <ul className="skills-list">
+            {technologies &&
+              technologies.map((skill, i) => <li key={i}>{skill.tech}</li>)}
+          </ul>
+        </StyledText>
+
+        <StyledPic>
+          <div className="wrapper">
+            <PrismicNextImage
+              className="img"
+              field={image}
+              height={300}
+              width={300}
+            />
+          </div>
+        </StyledPic>
+      </div>
+    </StyledAboutSection>
   );
 };
 
 export default AboutSection;
+
+const StyledAboutSection = styled.section`
+  max-width: 900px;
+
+  .inner {
+    display: grid;
+    grid-template-columns: 3fr 2fr;
+    grid-gap: 50px;
+
+    @media (max-width: 768px) {
+      display: block;
+    }
+  }
+`;
+const StyledText = styled.div`
+  ul.skills-list {
+    display: grid;
+    grid-template-columns: repeat(2, minmax(140px, 200px));
+    grid-gap: 0 10px;
+    padding: 0;
+    margin: 20px 0 0 0;
+    overflow: hidden;
+    list-style: none;
+
+    li {
+      position: relative;
+      margin-bottom: 10px;
+      padding-left: 20px;
+      font-family: var(--font-mono);
+      font-size: var(--fz-xs);
+
+      &:before {
+        content: "▹";
+        position: absolute;
+        left: 0;
+        color: var(--green);
+        font-size: var(--fz-sm);
+        line-height: 12px;
+      }
+    }
+  }
+`;
+const StyledPic = styled.div`
+  position: relative;
+  max-width: 300px;
+
+  @media (max-width: 768px) {
+    margin: 50px auto 0;
+    width: 70%;
+  }
+
+  .wrapper {
+    ${({ theme }) => theme.mixins.boxShadow};
+    display: block;
+    position: relative;
+    width: 100%;
+    border-radius: var(--border-radius);
+    background-color: var(--green);
+
+    &:hover,
+    &:focus {
+      outline: 0;
+      transform: translate(-4px, -4px);
+
+      &:after {
+        transform: translate(8px, 8px);
+      }
+
+      .img {
+        filter: none;
+        mix-blend-mode: normal;
+      }
+    }
+
+    .img {
+      position: relative;
+      border-radius: var(--border-radius);
+      mix-blend-mode: multiply;
+      filter: grayscale(100%) contrast(1);
+      transition: var(--transition);
+    }
+
+    &:before,
+    &:after {
+      content: "";
+      display: block;
+      position: absolute;
+      width: 100%;
+      height: 100%;
+      border-radius: var(--border-radius);
+      transition: var(--transition);
+    }
+
+    &:before {
+      top: 0;
+      left: 0;
+      background-color: var(--navy);
+      mix-blend-mode: screen;
+    }
+
+    &:after {
+      border: 2px solid var(--green);
+      top: 14px;
+      left: 14px;
+      z-index: -1;
+    }
+  }
+`;
