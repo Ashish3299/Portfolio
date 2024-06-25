dc51aaf7316789b4919833a0dc25e76e7c119ca2
Ashish <ashish15731@gmail.com>
2024-06-25 18:09:38 +0530

    Completed dynamic of the contact section slice.

--- src/slices/ContactSection/index.tsx
+++ src/slices/ContactSection/index.tsx
@@ -1,5 +1,10 @@
 import { Content } from "@prismicio/client";
-import { SliceComponentProps } from "@prismicio/react";
+import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
+import React, { useEffect, useRef } from "react";
+import usePrefersReducedMotion from "@/hooks/usePrefersReducedMotion";
+import { email } from "@/config";
+import styled from "styled-components";
+import Link from "next/link";
 
 /**
  * Props for `ContactSection`.
@@ -11,15 +16,65 @@ export type ContactSectionProps =
  * Component for "ContactSection" Slices.
  */
 const ContactSection = ({ slice }: ContactSectionProps): JSX.Element => {
+  const { subHeading, heading, description, CtaBtnText } = slice.primary;
+  const revealContainer = useRef(null);
+  const prefersReducedMotion = usePrefersReducedMotion();
+
+  useEffect(() => {
+    if (prefersReducedMotion) {
+      return;
+    }
+
+    // sr.reveal(revealContainer.current, srConfig());
+  }, []);
+
   return (
-    <section
-      data-slice-type={slice.slice_type}
-      data-slice-variation={slice.variation}
-    >
-      Placeholder component for contact_section (variation: {slice.variation})
-      Slices
-    </section>
+    <StyledContactSection id="contact" ref={revealContainer}>
+      <h2 className="numbered-heading overline">{subHeading}</h2>
+      <h2 className="title">{heading}</h2>
+      <PrismicRichText field={description} />
+      <Link className="email-link" href={`mailto:${email}`}>
+        {CtaBtnText}
+      </Link>
+    </StyledContactSection>
   );
 };
 
 export default ContactSection;
+
+const StyledContactSection = styled.section`
+  max-width: 600px;
+  margin: 0 auto 100px;
+  text-align: center;
+
+  @media (max-width: 768px) {
+    margin: 0 auto 50px;
+  }
+
+  .overline {
+    display: block;
+    margin-bottom: 20px;
+    color: var(--green);
+    font-family: var(--font-mono);
+    font-size: var(--fz-md);
+    font-weight: 400;
+
+    &:before {
+      bottom: 0;
+      font-size: var(--fz-sm);
+    }
+
+    &:after {
+      display: none;
+    }
+  }
+
+  .title {
+    font-size: clamp(40px, 5vw, 60px);
+  }
+
+  .email-link {
+    ${({ theme }) => theme.mixins.bigButton};
+    margin-top: 50px;
+  }
+`;
